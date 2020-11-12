2a7a7e4e8a958e1ad48f870acad93ec66d13978b
z-Juan-Pablo-z <juanpabloramirezflorez@gmail.com>
2020-11-12 18:32:55 -0500

    Diseño del fron en progreso

--- Frontend/src/FouticPage/Principal/Slider/Section.js
+++ Frontend/src/FouticPage/Principal/Slider/Section.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Primera from '../../img/ProductosPrimeraPagina/PrimerDescuento.png';
 import slider1 from '../../img/ProductosPrimeraPagina/slider1.png';
 import {Carousel} from 'react-bootstrap';
 
@@ -9,21 +10,21 @@ class Section extends Component {
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src={slider1}
+      src={Primera}
       alt="First slide"
     />
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src={slider1}
+      src={Primera}
       alt="Third slide"
     />
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src={slider1}
+      src={Primera}
       alt="Third slide"
     />
   </Carousel.Item>
